07463359979967cc6b9bd2bcf54a15e1c4431340
nicunursekatie <katielong2316@gmail.com>
2025-04-22 13:13:13 -0400

    Add GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/taskmanager-2/' // Assuming your repo is named taskmanager-2
+  base: '/taskmanager-2/'
 })
