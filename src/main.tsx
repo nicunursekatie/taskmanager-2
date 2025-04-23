bf1443c2000646dff25a03d1e6ad56b6c260c4de
nicunursekatie <katielong2316@gmail.com>
2025-04-23 12:40:22 -0400

    moved css to top

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import './compact-styles.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './compact-styles.css';
+
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
