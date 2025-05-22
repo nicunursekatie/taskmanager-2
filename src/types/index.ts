4c6bd8f8f5d83e37e9539b4f7c8007edfb2f999b
nicunursekatie <katielong2316@gmail.com>
2025-05-22 02:32:12 +0000

    Deploying to gh-pages from @ nicunursekatie/taskmanager-2@df49c2552ac39a4077f35692d3360532c3484500 🚀

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// Define and export the Category type
+export type Category = {
+    id: string;
+    name: string;
+    color: string;
+  };
+  
+  // Define and export the Project type
+  export type Project = {
+    id: string;
+    name: string;
+    description: string;
+  };
